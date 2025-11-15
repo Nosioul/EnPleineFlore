261b20ce41e0c321ac0d55d8e7f2832788454e42
Nosioul <Louison.charm@gmail.com>
2025-11-15 12:15:39 +0100

    add clarity tracking script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head>
+        {/* Favicon */}
         <link rel="icon" href="/favicon.png" />
+
+        {/* Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u6ghq07jr8");`,
+          }}
+        />
       </Head>
+
       <body>
         <Main />
         <NextScript />
